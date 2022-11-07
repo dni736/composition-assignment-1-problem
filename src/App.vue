5cfb9e23d9defbee7fb314ed30ace7ae89e9d2d5
dni736 <dima.nikolishvili@gmail.com>
2022-11-07 15:19:15 +0400

    submit task (options API)

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <template>
-  <h2>My Course Goal</h2>
+  <h2>My Internship Goals</h2>
   <!-- Task 1: Output your main course goal with help of the composition API -->
   <!-- Don't hardcode it into the template, instead hardcode it into the JS code -->
-  <h3>OUTPUT COURSE GOAL</h3>
+  <div id="goals" v-show="toggle">
+    <h3 :key="id" v-for="(goal, id) in goals">{{ goal }}</h3>
+  </div>
   <!-- Task 2: Toggle (show/ hide) the goal with help of the button  -->
-  <button>Toggle Goal</button>
+  <button @click="hideOrShow">{{ caption }}</button>
   <!-- Task 3: Manage data in three ways -->
   <!-- => Separate refs -->
   <!-- => Ref Object -->
@@ -13,7 +15,23 @@
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      goals: ['Vue', 'Nuxt', 'Magento'],
+      toggle: true,
+      caption: 'Hide',
+    };
+  },
+  methods: {
+    hideOrShow() {
+      this.toggle = !this.toggle;
+      this.caption == 'Hide'
+        ? (this.caption = 'Show')
+        : (this.caption = 'Hide');
+    },
+  },
+};
 </script>
 
 <style>
