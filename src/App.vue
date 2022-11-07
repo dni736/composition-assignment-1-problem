c42ef036df6d5f3a1937ea224294b2616f224291
dni736 <dima.nikolishvili@gmail.com>
2022-11-07 20:02:08 +0400

    submit task (composition API)

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,13 @@
   <h2>My Internship Goals</h2>
   <!-- Task 1: Output your main course goal with help of the composition API -->
   <!-- Don't hardcode it into the template, instead hardcode it into the JS code -->
-  <div id="goals" v-show="toggle">
-    <h3 :key="id" v-for="(goal, id) in goals">{{ goal }}</h3>
+  <div id="goals" v-show="visible">
+    <h3>{{ goalOne }}</h3>
+    <h3>{{ goalTwo }}</h3>
+    <h3>{{ goalThree }}</h3>
   </div>
   <!-- Task 2: Toggle (show/ hide) the goal with help of the button  -->
-  <button @click="hideOrShow">{{ caption }}</button>
+  <button @click="toggle">{{ caption }}</button>
   <!-- Task 3: Manage data in three ways -->
   <!-- => Separate refs -->
   <!-- => Ref Object -->
@@ -15,21 +17,32 @@
 </template>
 
 <script>
+import { ref, reactive } from 'vue';
 export default {
-  data() {
+  setup() {
+    var visible = ref(true);
+    var caption = ref('Hide');
+    const goal1 = ref('Vue');
+    const internshipGoalRefObj = ref({
+      goal2: 'Nuxt',
+    });
+    const internshipGoalReactObj = reactive({
+      goal3: 'Magento',
+    });
+    function hideOrShow() {
+      visible.value = !visible.value;
+      caption.value == 'Hide'
+        ? (caption.value = 'Show')
+        : (caption.value = 'Hide');
+    }
     return {
-      goals: ['Vue', 'Nuxt', 'Magento'],
-      toggle: true,
-      caption: 'Hide',
+      visible,
+      caption,
+      goalOne: goal1,
+      goalTwo: internshipGoalRefObj.value.goal2,
+      goalThree: internshipGoalReactObj.goal3,
+      toggle: hideOrShow,
     };
-  },
-  methods: {
-    hideOrShow() {
-      this.toggle = !this.toggle;
-      this.caption == 'Hide'
-        ? (this.caption = 'Show')
-        : (this.caption = 'Hide');
-    },
   },
 };
 </script>
